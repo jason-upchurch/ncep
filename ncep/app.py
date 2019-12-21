0bf80619cb192016635eaa61a390db9383e8b3ac
John Carroll <j.john.carroll@gmail.com>
2019-12-21 04:58:22 +0000

    delete __app__.py, create app.py

---
+++ ncep/app.py
@@ -0,0 +1,100 @@
+from flask import Flask
+from flask import render_template
+import io
+import base64
+
+app = Flask(__name__)
+
+
+#@app.route('/')
+#def hello():
+
+from datetime import datetime
+import ncep_util
+import ncep_mapping
+
+
+
+'''
+user configuration:
+
+path: enter a path to save your maps and data (not required, but if the
+      "savepath" argument is passed, path must be a valid directory or
+      the boolean False))
+email: enter your email address (optional)
+'''
+
+
+'''
+map average temps:
+
+fname is the filename you wish to save as (this will apply to the csv and
+the png that are saved. Do not provide an extension--both files will get
+the same name with csv and png extensions.
+'''
+# fname1 = 'average_temps'
+# date_start = datetime(2019,1,29).date() #provide a valid start date YYYY, (M)M, (D)D
+# date_end = datetime(2019,2,2).date()  #provide a valid start date YYYY, (M)M, (D)D
+# plt_average_temps = ncep_mapping.map_average_temps(date_start, date_end, '',
+#                                       split_on = 55,how='state',legend=True,savepath=False)
+# plt_average_temps.show()
+
+
+
+'''
+map compare average temps
+'''
+@app.route('/')
+def hello():
+    fname2 = 'compare_average_temps'
+    savepath = True
+    date_start = datetime(2018,1,1).date()
+    date_end = datetime(2018,1,1).date()
+    date_start_compare = datetime(2014,1,7).date()
+    date_end_compare = datetime(2014,1,7).date()
+    plt_compare = ncep_mapping.map_compare_average_temps(date_start, date_end, date_start_compare, date_end_compare,
+                    '', how='stcd',legend = True)
+    #plt_compare.show()
+    img = io.BytesIO()
+    plt_compare.savefig(img, format='png')
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    #myimg = '<img src="data:image/png;base64,{}">'.format(plot_url)
+
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
+    #return render_template('index.html', plt=myimg)
+
+
+
+'''
+map departure from normal temps
+'''
+# path = ''
+# fname3 = 'Jan_30_departures'
+# savepath = path + fname3
+# date_start = datetime(2019,2,1).date()
+# date_end = datetime(2019,2,26).date()
+# begin_normal_year, end_normal_year = 1981, 2010
+# plt_depart_from_normal = ncep_mapping.map_depart_from_normal_temps(
+#                             date_start, date_end, begin_normal_year, end_normal_year,
+#                             '', how='state', legend = True, savepath=False)
+# plt_depart_from_normal.show()
+
+
+
+'''
+advanced: get range of heating degree days for state (illustrates interaction with
+          ncep_util module
+'''
+# fname4 = 'default'
+# date_start = datetime(2016,1,1).date()
+# date_end = datetime(2016,12,31).date()
+# HDD_FNAME_STATE = 'StatesCONUS.Heating.txt'
+# hdd = ncep_util.retrieve_range(HDD_FNAME_STATE, date_start, date_end, '')
+# hdd.to_csv(path+fname4)
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, debug = True)
